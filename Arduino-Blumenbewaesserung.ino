6b3a49c54d464a61e86fd8576b60e8144aad5e78
Bastian-Reul <45174161+Bastian-Reul@users.noreply.github.com>
2019-03-26 10:44:48 +0100

    Erste Kommentare wurden eingefügt.

---
+++ Arduino-Blumenbewaesserung.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // Hier werden die Ein und Ausgänge des HIMALAYA basic UNO R3 ATmega328P Board definiert:
+
+}
+
+void loop() {
+  // Die Hauptschleife:
+
+}
+
+//Die Timer-Schleife, die regelmäßig angesprungen wird
